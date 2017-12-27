2131b1170a311893387cd4ad92346f2fbada69f8
Alexander Grigorev <vill05@mail.ru>
2017-12-27 09:44:22 +0700

    Fix Rubocop offenses

--- lib/devise_pkcs12_authenticatable.rb
+++ lib/devise_pkcs12_authenticatable.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'devise'
 require 'active_support'
 require 'active_support/inflector'
@@ -5,18 +7,13 @@ require 'active_support/concern'
 
 require 'devise_pkcs12_authenticatable/engine'
 
-module DevisePKCS12Authenticatable
-end
-
 module Devise
   # Name of the field for 'commonName'
   mattr_accessor :pkcs12_common_name_field
-  @@pkcs12_common_name_field = :cn
+  @@pkcs12_common_name_field = :cn # rubocop:disable Style/ClassVars
 end
 
-Devise.add_module(:pkcs12_authenticatable,
-  route: :session,
-  controller: :sessions,
-  strategy: true,
-  model: 'devise_pkcs12_authenticatable/model'
-)
+Devise.add_module(:pkcs12_authenticatable, route: :session,
+                                           controller: :sessions,
+                                           strategy: true,
+                                           model: 'devise_pkcs12_authenticatable/model')
